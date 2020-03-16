049645e669e3fabb17d5a94f07c75a6642dd75e8
Florian Schlickenrieder <florian.schlickenrieder@gmx.ch>
2020-03-16 11:52:16 +0100

    Create basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartComponent } from './cart/cart.component';
+import { ContentComponent } from './content/content.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
+import { ProductItemComponent } from './product-item/product-item.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    ContentComponent,
+    NavigationComponent,
+    SidebarComponent,
+    HeaderComponent,
+    ProductItemComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
