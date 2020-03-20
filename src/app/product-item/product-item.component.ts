8aa7bda3a2a861f321fd35772c776a5c98b28329
Florian Schlickenrieder <florian.schlickenrieder@gmx.ch>
2020-03-20 14:11:44 +0100

    Add mock data and display it

--- src/app/product-item/product-item.component.ts
+++ src/app/product-item/product-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-product-item',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductItemComponent implements OnInit {
 
+  @Input() item: object;
   constructor() { }
 
   ngOnInit() {
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -7,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
+  data = [];
   constructor() { }
 
   ngOnInit() {
+    // http request get data
+    var mockData = [
+      {
+        "name": "Gibson Les Paul Signature 2020",
+        "desc": "Insert description",
+        "price": 3299.90,
+      },
+      {
+        "name": "Gibson Les Paul Signature 2019",
+        "desc": "Insert description",
+        "price": 3289.90,
+      },
+      {
+        "name": "Gibson Les Paul Signature 2018",
+        "desc": "Insert description",
+        "price": 3249.90,
+      },
+      {
+        "name": "Gibson Les Paul Signature 2017",
+        "desc": "Insert description",
+        "price": 3199.90,
+      },
+      {
+        "name": "Gibson Les Paul Signature 2016",
+        "desc": "Insert description",
+        "price": 3399.90,
+      },
+    ];
+
+    this.data = mockData;
   }
 
 }
